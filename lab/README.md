bb94d2d9aae0d4e7d96f942cfbb212d217b64c1a
Norbert <norbert.niderla@gmail.com>
2021-10-23 11:06:14 +0200

    lab folder added

---
+++ lab/README.md
@@ -0,0 +1,9 @@
+lab
+===
+
+Describe here what the application is about.
+
+Usage
+=====
+
+Describe here how to use this application
---
+++ lab/main.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2021 Warsaw University of Technology
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       mcu programming (RIOT OS) - threading and irq
+ *
+ * @author      Norbert Niderla <norbert.niderla@gmail.com>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    puts("Generated RIOT application: 'lab'");
+    return 0;
+}
